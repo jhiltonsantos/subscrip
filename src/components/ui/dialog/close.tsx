dfd5dd6a00e95b19a20abb2a697325bc4a172464
Hilton Santos <jhilton930@gmail.com>
2026-07-03 21:34:09 -0300

    refactor: remove Dialog component and its related subcomponents to streamline UI codebase

---
+++ src/components/ui/dialog/close.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import * as React from "react"
+import * as DialogPrimitive from "@radix-ui/react-dialog"
+
+function DialogClose({
+  ...props
+}: React.ComponentProps<typeof DialogPrimitive.Close>) {
+  return <DialogPrimitive.Close data-slot="dialog-close" {...props} />
+}
+
+export { DialogClose }
